24991a357742930aecb41f47fe350f5c7db81f2e
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2014-02-13 17:56:16 -0500

    Start project

---
+++ faraday/settings/base.py
@@ -0,0 +1,33 @@
+from unipath import Path
+PROJECT_DIR = Path(__file__).ancestor(2)
+
+SECRET_KEY = '=+t8w&x0y*!yzpyy1i@m!@!g%%cfxx^byq_7&s5i8f#7llg60v'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'faraday.urls'
+WSGI_APPLICATION = 'faraday.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
