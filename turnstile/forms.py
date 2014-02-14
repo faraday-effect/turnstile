a3698b6600b2460eb9c96fce0186f84893bf2c5d
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2014-02-13 23:14:42 -0500

    Got login and account creation working

--- turnstile/forms.py
+++ turnstile/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Student
+
 class LoginForm(forms.Form):
     user_name = forms.CharField(max_length=20)
     password = forms.CharField(max_length=20, widget=forms.PasswordInput())
@@ -10,3 +12,19 @@ class AccountForm(forms.Form):
     password = forms.CharField(max_length=20, widget=forms.PasswordInput())
     repeat_password = forms.CharField(max_length=20, widget=forms.PasswordInput())
 
+    def clean_user_name(self):
+        """Make sure no user with the same user_name."""
+        user = self.cleaned_data.get("user_name")
+        try:
+            Student.objects.get(user_name=user)
+        except Student.DoesNotExist:
+            return user
+        raise forms.ValidationError("Already a student with that user name")
+
+    def clean_repeat_password(self):
+        """Make sure the passwords match one another."""
+        pass1 = self.cleaned_data.get("password")
+        pass2 = self.cleaned_data.get("repeat_password")
+        if pass1 != pass2:
+            raise forms.ValidationError("Passwords don't match")
+        return pass2
--- turnstile/views.py
+++ turnstile/views.py
@@ -3,13 +3,21 @@ from django.contrib.auth.hashers import make_password, check_password
 from django.shortcuts import render, redirect
 
 from .forms import *
+from .models import *
 
 def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            print "GOT A FORM"
-            pass
+            user = form.cleaned_data.get("user_name")
+            try:
+                student = Student.objects.get(user_name=user)
+                if not check_password(form.cleaned_data.get("password"), student.password):
+                    messages.error(request, "Wrong password")
+                else:
+                    return redirect('turnstile_assignments')
+            except Student.DoesNotExist:
+                messages.error(request, "That user name doesn't exist")
     else:
         form = LoginForm()
     return render(request, 'turnstile/login.html', { 'form': form })
@@ -18,11 +26,12 @@ def add_account(request):
     if request.method == 'POST':
         form = AccountForm(request.POST)
         if form.is_valid():
-            if form.cleaned_data['password'] != form.cleaned_data['repeat_password']:
-                messages.error(request, "Passwords didn't match")
-            else:
-                messages.success(request, "Account created")
-                return redirect('turnstile_assignments')
+            student = Student(full_name = form.cleaned_data.get("full_name"),
+                              user_name = form.cleaned_data.get("user_name"),
+                              password = make_password(form.cleaned_data.get("password")))
+            student.save()
+            messages.success(request, "Account created")
+            return redirect('turnstile_assignments')
     else:
         form = AccountForm()
     return render(request, 'turnstile/add_account.html', { 'form': form })
